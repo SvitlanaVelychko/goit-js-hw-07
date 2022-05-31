b4a56f6882fbe494633b6691c7de4a8a468c4f50
SvitlanaVelychko <svel2608@gmail.com>
2022-05-31 22:17:55 +0300

    Solve tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = onCreateGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryImageClick);
+
+function onCreateGalleryItemsMarkup(items) {
+    return items
+        .map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </div>`;
+        })
+        .join('');
+}
+
+function onGalleryImageClick(e) {
+    e.preventDefault();
+
+    const image = e.target;
+
+    if (!image.classList.contains('gallery__image')) {
+        return;
+    }
+
+    console.log(image.dataset.source);
+}
 
 console.log(galleryItems);
